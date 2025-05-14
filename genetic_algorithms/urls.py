04c12758df8fd57007768f7f34a333b3eeb39c4f
Diego Alvarez <dalvarez@bitz.pe>
2025-05-13 19:25:05 -0500

    feat: delete unused files

--- genetic_algorithms/urls.py
+++ genetic_algorithms/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('ejecutar-algoritmo/', views.ejecutar_algoritmo, name='ejecutar_algoritmo'),
-    path('servicios/', views.servicios, name='servicios'),
-    path('contacto/', views.contacto, name='contacto'),
 ]
